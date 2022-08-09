429a0d21938af8e70eb3c642f068e446261ad7c1
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-08-09 15:51:17 +0900

    feat: i/o attempt numbers

--- rust-racingcar/src/main.rs
+++ rust-racingcar/src/main.rs
@@ -2,9 +2,14 @@
 
 fn main() {
     println!("Rust로 구현하는 자동차 경주 게임");
-    println!("자동차 대수는 몇 대인가요?");
 
     // 자동차 대수를 입력받는다.
+    println!("자동차 대수는 몇 대인가요?");
     let the_number_of_cars: i32 = input_integer::input();
-    println!("{:?}", the_number_of_cars);
+    println!("입력받은 자동차 대수: {:?}", the_number_of_cars);
+
+    // 시도할 횟수를 입력받는다.
+    println!("시도할 횟수는 몇 회인가요?");
+    let the_number_of_attempts: i32 = input_integer::input();
+    println!("시도할 횟수: {:?}", the_number_of_attempts);
 }
