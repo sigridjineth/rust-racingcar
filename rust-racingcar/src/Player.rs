e34db6ec81ee7e06bfb4cf5ed042be808d7e5057
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-04 16:20:22 +0900

    refactor: Player to print by current round number

--- rust-racingcar/src/Player.rs
+++ rust-racingcar/src/Player.rs
@@ -12,16 +12,33 @@ impl Player {
             steps: Vec::with_capacity(number_of_attempts as usize)
         }
     }
+
     pub fn play_steps(&mut self) {
-        for _ in 0..self.steps.capacity() {
-            let step = step::Step::new();
+        for step_number in 0..self.steps.capacity() {
+            // get last step
+            let last_step = self.steps.last();
+            let step = step::Step::new(step_number as i32, last_step);
             self.steps.push(step);
         }
     }
+
     pub fn print_the_dash_by_the_amount_of_is_moved_on_steps(&self) {
         println!("{}", self.name);
         for step in self.steps.iter() {
             println!("{}", step);
         }
+    }
+
+    pub fn print_the_dash_by_the_amount_of_steps_on_this_step(&self, step_number: i32) {
+        for step in self.steps.iter() {
+            if step.get_step_number() == step_number {
+                println!("{}: {}", self.name, step);
+                break;
+            }
+        }
+    }
+
+    pub fn get_player_name(&self) -> String {
+        self.name.to_string()
     }
 }
