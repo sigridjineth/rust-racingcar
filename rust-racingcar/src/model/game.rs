6197e0339305d12925eff4e8638018dc518377e8
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-04 19:25:56 +0900

    refactor: implement trait GameFn

--- rust-racingcar/src/model/game.rs
+++ rust-racingcar/src/model/game.rs
@@ -1,14 +1,28 @@
-#[path = "Player.rs"]
+#[path = "player.rs"]
 pub(crate) mod player;
+
 #[derive(Debug)]
 pub struct Game {
     pub number_of_players: i32,
     pub number_of_attempts: i32,
-    players: Vec<player::Player>
+    pub(crate) players: Vec<player::Player>
 }
 
-impl Game {
-    pub fn new(name_of_players: Vec<String>, number_of_players: i32, number_of_attempts: i32) -> Self {
+pub trait GameFn {
+    fn new(player_names: Vec<String>, the_number_of_cars: i32, the_number_of_attempts: i32) -> Self;
+    fn print_the_dash_by_the_amount_of_is_moved_on_steps(&self);
+    fn print_the_dash_by_the_amount_of_steps_on_this_step(&self, step_number: i32);
+    fn get_players(&self) -> &Vec<player::Player>;
+    fn get_number_of_players(&self) -> i32;
+    fn get_number_of_attempts(&self) -> i32;
+    fn initialize_players(self, player_names: &Vec<String>) -> Self;
+    fn play_steps(self) -> Self;
+}
+
+use mockall::automock;
+#[automock]
+impl GameFn for Game {
+    fn new(name_of_players: Vec<String>, number_of_players: i32, number_of_attempts: i32) -> Self {
         let new_game = Self {
             number_of_players,
             number_of_attempts,
@@ -22,19 +36,34 @@ impl Game {
         new_game
     }
 
-    pub fn get_number_of_players(&self) -> i32 {
-        self.number_of_players
+    // TODO: refactor to output UI
+    fn print_the_dash_by_the_amount_of_is_moved_on_steps(&self) {
+        for player in self.players.iter() {
+            player.print_the_dash_by_the_amount_of_is_moved_on_steps();
+        }
     }
 
-    pub fn get_number_of_attempts(&self) -> i32 {
-        self.number_of_attempts
+    // TODO: refactor to output UI
+    fn print_the_dash_by_the_amount_of_steps_on_this_step(&self, step_number: i32) {
+        for player in self.players.iter() {
+            player.print_the_dash_by_the_amount_of_steps_on_this_step(step_number);
+        }
+        println!();
     }
 
-    pub fn get_players(&self) -> &Vec<player::Player> {
+    fn get_players(&self) -> &Vec<player::Player> {
         &self.players
     }
 
-    pub fn initialize_players(mut self, name_of_players: &Vec<String>) -> Self {
+    fn get_number_of_players(&self) -> i32 {
+        self.number_of_players
+    }
+
+    fn get_number_of_attempts(&self) -> i32 {
+        self.number_of_attempts
+    }
+
+    fn initialize_players(mut self, name_of_players: &Vec<String>) -> Self {
         for name in name_of_players {
             let new_player = player::Player::new(name.to_string(), self.number_of_attempts);
             self.players.push(new_player);
@@ -42,24 +71,11 @@ impl Game {
         self
     }
 
-    pub fn play_steps(self) -> Self {
+    fn play_steps(self) -> Self {
         let mut this = self;
         for player in this.players.iter_mut() {
             player.play_steps();
         }
         this
-    }
-
-    pub fn print_the_dash_by_the_amount_of_is_moved_on_steps(&self) {
-        for player in self.players.iter() {
-            player.print_the_dash_by_the_amount_of_is_moved_on_steps();
-        }
-    }
-
-    pub fn print_the_dash_by_the_amount_of_steps_on_this_step(&self, step_number: i32) {
-        for player in self.players.iter() {
-            player.print_the_dash_by_the_amount_of_steps_on_this_step(step_number);
-        }
-        println!();
     }
 }
