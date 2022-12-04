516427588c58abc45014fd369f1eed78ef04916b
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-04 20:50:56 +0900

    test: [WIP] test_mocked_game_new_function

--- rust-racingcar/src/test/game-test.rs
+++ rust-racingcar/src/test/game-test.rs
@@ -4,8 +4,7 @@ use std::panic;
 use game::player;
 use crate::game::{Game as TestGame, Game};
 use crate::game_test;
-use crate::game::GameFn;
-use crate::game_test::game::GameFn as testGameFn;
+use crate::game::{GameFn, MockGameFn};
 use crate::game::player::step;
 
 fn run_test<T>(test: T)
@@ -63,47 +62,59 @@ fn is_able_to_create_new_step_struct_in_player() {
 
 #[test]
 fn test_mocked_game_new_function() {
-        let step_case_1 = step::Step {
-            step_number: 1,
-            is_moved: 3,
-        };
-        let step_case_1_optional = Some(&step_case_1);
+    let step_case_1 = step::Step {
+        step_number: 1,
+        is_moved: 3,
+    };
+    let step_case_1_optional = Some(&step_case_1);
 
-        let mut step_case_2 = step::Step {
-            step_number: 2,
-            is_moved: 4,
-        };
-        step_case_2.update_this_step_move_number(step_case_2.is_moved, step_case_1_optional);
-        let step_case_2_optional = Some(&step_case_2);
+    let mut step_case_2 = step::Step {
+        step_number: 2,
+        is_moved: 4,
+    };
+    step_case_2.update_this_step_move_number(step_case_2.is_moved, step_case_1_optional);
+    let step_case_2_optional = Some(&step_case_2);
 
-        let mut step_case_3 = step::Step {
-            step_number: 3,
-            is_moved: 5,
-        };
-        step_case_3.update_this_step_move_number(step_case_3.is_moved, step_case_2_optional);
-        let step_case_3_optional = Some(&step_case_3);
+    let mut step_case_3 = step::Step {
+        step_number: 3,
+        is_moved: 5,
+    };
+    step_case_3.update_this_step_move_number(step_case_3.is_moved, step_case_2_optional);
+    let step_case_3_optional = Some(&step_case_3);
 
-        let mut step_case_4 = step::Step {
-            step_number: 4,
-            is_moved: 2,
-        };
-        step_case_4.update_this_step_move_number(step_case_4.is_moved, step_case_3_optional);
-        let step_case_4_optional = Some(&step_case_4);
+    let mut step_case_4 = step::Step {
+        step_number: 4,
+        is_moved: 2,
+    };
+    step_case_4.update_this_step_move_number(step_case_4.is_moved, step_case_3_optional);
+    let step_case_4_optional = Some(&step_case_4);
 
-        let mut step_case_5 = step::Step {
-            step_number: 5,
-            is_moved: 0,
-        };
-        step_case_5.update_this_step_move_number(step_case_5.is_moved, step_case_4_optional);
+    let mut step_case_5 = step::Step {
+        step_number: 5,
+        is_moved: 0,
+    };
+    step_case_5.update_this_step_move_number(step_case_5.is_moved, step_case_4_optional);
+
+    let players_name = vec!["pobi".to_string(), "crong".to_string(), "honux".to_string()];
+
+    // https://github.com/asomers/mockall/issues/44
+    let ctx = MockGameFn::new_context();
+    ctx.expect().returning(|_, _, _| MockGameFn::default());
+
+    let mut mock_foo = MockGameFn::new(players_name, 3, 5);
+    mock_foo.expect_initialize_players()
+        .returning(|_| MockGameFn::default());
+    mock_foo.expect_get_number_of_attempts()
+        .returning(|| 5);
+    mock_foo.expect_get_number_of_players()
+        .returning(|| 3);
+
+    // let player_names = vec!["pobi".to_string(), "crong".to_string()];
+    // let number_of_players = 2;
+    // let number_of_attempts = 5;
+    // let game = mock.new(player_names, number_of_players, number_of_attempts);
+    // assert_eq!(game.number_of_players, 2);
+    // assert_eq!(game.number_of_attempts, 5);
+    // assert_eq!(game.players.len(), 2);
 }
 
-
-
-
-// let players_name = vec!["pobi".to_string(), "crong".to_string(), "honux".to_string()];
-//         let number_of_players: i32 = players_name.len() as i32;
-//         let number_of_attempts: i32 = 5;
-//
-//         let mut mock = MockGame::new_mock_game(players_name, number_of_players, number_of_attempts);
-//         assert_eq!(mock.get_number_of_players(), 3);
-//         assert_eq!(mock.get_number_of_attempts(), 5);
